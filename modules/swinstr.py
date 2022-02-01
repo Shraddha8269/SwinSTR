86aed800f0e11bb1e3c480d113fdec3070748f7c
Shradha Jain <44806436+Shraddha8269@users.noreply.github.com>
2022-02-01 08:40:32 +0530

    Create swinstr.py

---
+++ modules/swinstr.py
@@ -0,0 +1,141 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch 
+import torch.nn as nn
+import logging
+import torch.utils.model_zoo as model_zoo
+
+from copy import deepcopy
+from functools import partial
+from timm.models.swin_transformer import SwinTransformer, _cfg
+from timm.models.registry import register_model
+from timm.models import create_model
+import timm
+
+_logger = logging.getLogger(__name__)
+
+
+def create_swinstr(num_tokens, model=None, checkpoint_path=''):
+    swinstr = create_model(
+        model,
+        pretrained=True,
+        num_classes=num_tokens,
+        checkpoint_path=checkpoint_path)
+
+    # might need to run to get zero init head for transfer learning
+    swinstr.reset_classifier(num_classes=num_tokens)
+
+    return swinstr
+
+class SWINSTR(SwinTransformer):
+   
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    
+    def reset_classifier(self, num_classes):
+        self.num_classes = num_classes
+        self.embed_dim=768
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        #cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        #x = torch.cat((cls_tokens, x), dim=1)
+        #x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.layers:
+            x = blk(x)
+
+        x = self.norm(x)
+        return x
+
+    def forward(self, x, seqlen=25):
+        x = self.forward_features(x)
+        x = x[:, :seqlen]
+
+        # batch, seqlen, embsize
+        b, s, e = x.size()
+        x = x.reshape(b*s, e)
+        x = self.head(x).view(b, s, self.num_classes)
+        return x
+
+
+def load_pretrained(model, cfg=None, num_classes=1000, in_chans=1, filter_fn=None, strict=True):
+   
+    if cfg is None:
+        cfg = getattr(model, 'default_cfg')
+    if cfg is None or 'url' not in cfg or not cfg['url']:
+        _logger.warning("Pretrained model URL is invalid, using random initialization.")
+        return
+
+    state_dict = model_zoo.load_url(cfg['url'], progress=True, map_location='cpu')
+    if "model" in state_dict.keys():
+        state_dict = state_dict["model"]
+
+    #if filter_fn is not None:
+     #   state_dict = filter_fn(state_dict)
+
+    if in_chans == 1:
+        conv1_name = cfg['first_conv']
+        _logger.info('Converting first conv (%s) pretrained weights from 3 to 1 channel' % conv1_name)
+        key = conv1_name + '.weight'
+        if key in state_dict.keys():
+            _logger.info('(%s) key found in state_dict' % key)
+            conv1_weight = state_dict[conv1_name + '.weight']
+        else:
+            _logger.info('(%s) key NOT found in state_dict' % key)
+            return
+        conv1_type = conv1_weight.dtype
+        conv1_weight = conv1_weight.float()
+        O, I, J, K = conv1_weight.shape
+        if I > 3:
+            assert conv1_weight.shape[1] % 3 == 0
+            conv1_weight = conv1_weight.reshape(O, I // 3, 3, J, K)
+            conv1_weight = conv1_weight.sum(dim=2, keepdim=False)
+        else:
+            conv1_weight = conv1_weight.sum(dim=1, keepdim=True)
+        conv1_weight = conv1_weight.to(conv1_type)
+        state_dict[conv1_name + '.weight'] = conv1_weight
+
+    classifier_name = cfg['classifier']
+    if num_classes == 1000 and cfg['num_classes'] == 1001:
+        classifier_weight = state_dict[classifier_name + '.weight']
+        state_dict[classifier_name + '.weight'] = classifier_weight[1:]
+        classifier_bias = state_dict[classifier_name + '.bias']
+        state_dict[classifier_name + '.bias'] = classifier_bias[1:]
+    elif num_classes != cfg['num_classes']:
+        del state_dict[classifier_name + '.weight']
+        del state_dict[classifier_name + '.bias']
+        strict = False
+
+    print("Loading pre-trained vision transformer weights from %s ..." % cfg['url'])
+    model.load_state_dict(state_dict, strict=strict)
+
+
+def _conv_filter(state_dict, patch_size=16):
+    """ convert patch embedding weight from manual patchify + linear proj to conv"""
+    out_dict = {}
+    print(state_dict)
+    for k, v in state_dict.items():
+        if 'patch_embed.proj.weight' in k:
+            v = v.reshape((v.shape[0], 3, patch_size, patch_size))
+        out_dict[k] = v
+    return out_dict
+@register_model
+def swinstr_small_patch16_224(pretrained=False, **kwargs):
+    kwargs['in_chans'] = 1
+    model = SWINSTR(
+       patch_size=4, window_size=7, embed_dim=96, depths=(2, 2, 18, 2), num_heads=(3, 6, 12, 24), **kwargs)
+    model.default_cfg = _cfg(
+            url='https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth'
+    )
+    if pretrained:
+        load_pretrained(
+            model, num_classes=model.num_classes, in_chans=kwargs.get('in_chans', 1), filter_fn=_conv_filter)
+    return model
