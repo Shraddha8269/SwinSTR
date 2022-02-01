913c71e3fa14e72d741f83027b29eb0944f98452
Shradha Jain <44806436+Shraddha8269@users.noreply.github.com>
2022-02-01 08:34:53 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import math
+
+class Model(nn.Module):
+
+    def __init__(self, opt):
+        super(Model, self).__init__()
+        self.opt = opt
+        self.swinstr= create_swinstr(num_tokens=opt.num_class, model=opt.TransformerModel)
+        return
+
+    def forward(self, input, text, is_train=True, seqlen=25):
+            prediction = self.swinstr(input, seqlen=seqlen)
+            return prediction
