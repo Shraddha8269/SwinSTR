1e9c639775dc31f1386c326b5c2f13d55e4f98fe
Shradha Jain <44806436+Shraddha8269@users.noreply.github.com>
2022-02-24 14:22:51 +0530

    Update swinstr2.py

--- modules/swinstr2.py
+++ modules/swinstr2.py
@@ -684,7 +684,7 @@ def _conv_filter(state_dict, patch_size=16):
 @register_model
 def swinstr_small_patch16_224(pretrained=False, **kwargs):
     kwargs['in_chans'] = 1
-    model = SWINSTR(
+    model = SwinTransformer(
        patch_size=4, window_size=7, embed_dim=96, depths=(2, 2, 18, 2), num_heads=(3, 6, 12, 24), **kwargs)
     model.default_cfg = _cfg(
             url='https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth'
@@ -696,7 +696,7 @@ def swinstr_small_patch16_224(pretrained=False, **kwargs):
 @register_model
 def swinstr_base_patch16_224(pretrained=False, **kwargs):
     kwargs['in_chans'] = 1
-    model = SWINSTR(
+    model = SwinTransformer(
          patch_size=4, window_size=7, embed_dim=128, depths=(2, 2, 18, 2), num_heads=(4, 8, 16, 32), **kwargs)
     model.default_cfg = _cfg(
             #url='https://github.com/roatienza/public/releases/download/v0.1-deit-base/deit_base_patch16_224-b5f2ef4d.pth'
