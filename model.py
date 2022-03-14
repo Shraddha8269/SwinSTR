4b4cb1f042595a57945e8608081e428d77ddda6a
Shradha Jain <44806436+Shraddha8269@users.noreply.github.com>
2022-03-14 11:56:05 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class Model(nn.Module):
     def __init__(self, opt):
         super(Model, self).__init__()
         self.opt = opt
-        self.swinstr= create_swinstr(num_tokens=opt.num_class, model=opt.TransformerModel)
+        self.pvtstr= create_pvtstr(num_tokens=opt.num_class, model=opt.TransformerModel)
         return
 
     def forward(self, input, text, is_train=True, seqlen=25):
