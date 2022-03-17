301bb51984df22d9a4abe11067c01c63528757a8
Shradha Jain <44806436+Shraddha8269@users.noreply.github.com>
2022-03-17 16:03:54 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,16 +1,16 @@
 import torch
 import torch.nn as nn
 import math
-from modules.pvt_str import create_pvtstr
+from modules.uni_str import create_unistr
 
 class Model(nn.Module):
 
     def __init__(self, opt):
         super(Model, self).__init__()
         self.opt = opt
-        self.pvtstr= create_pvtstr(num_tokens=opt.num_class, model=opt.TransformerModel)
+        self.unistr= create_unistr(num_tokens=opt.num_class, model=opt.TransformerModel)
         return
 
     def forward(self, input, text, is_train=True, seqlen=25):
-            prediction = self.pvtstr(input, seqlen=seqlen)
+            prediction = self.unistr(input, seqlen=seqlen)
             return prediction
